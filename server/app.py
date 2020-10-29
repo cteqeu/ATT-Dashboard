4ba0de32d7f0888814fa6da23c146d91e6c167cc
Jens Vanhulst <vanhulstjens@gmail.com>
2020-10-29 11:24:42 +0100

    working server application

--- server/app.py
+++ server/app.py
@@ -1,15 +1,11 @@
+import eventlet
+import json
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit, ConnectionRefusedError
-from flask_cors import CORS, cross_origin
-import paho.mqtt.subscribe as subscribe
-import paho.mqtt.client as mqtt
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+from flask_bootstrap import Bootstrap
 
-BROKER_URL = "api.allthingstalk.io"
-BROKER_PORT = 1883
-ATT_DEVICE_TOKEN = "maker:4H0f8zZfr9kSG0lqFujuiTclythuwmZx1ZNiMo8"
-ATT_DEVICE_ID = "fO7K0gYRpEMJmY1PDy5cSpzh"
-ATT_SUB_TOPIC ="device/" + ATT_DEVICE_ID + "/asset/#"
-ATT_TEMP_SUB_TOPIC = "device/" + ATT_DEVICE_ID + "/asset/temperature/feed"
+eventlet.monkey_patch()
 
 # Smartphone has following assets:
 #   battery         (sensor)    => number
@@ -21,37 +17,71 @@ ATT_TEMP_SUB_TOPIC = "device/" + ATT_DEVICE_ID + "/asset/temperature/feed"
 #   location        (location)  => object (lat number, long number)
 #   shake           (location)  => boolean
 
+SECRET = 'cQfTjWnZr4u7x!A%D*G-KaPdSgUkXp2s'
+BROKER_URL = "api.allthingstalk.io"
+BROKER_PORT = 1883
+ATT_DEVICE_TOKEN = "maker:4H0f8zZfr9kSG0lqFujuiTclythuwmZx1ZNiMo8"
+ATT_DEVICE_ID = "fO7K0gYRpEMJmY1PDy5cSpzh"
+ATT_SUB_TOPIC ="device/" + ATT_DEVICE_ID + "/asset/#"
+ATT_TEMP_SUB_TOPIC = "device/" + ATT_DEVICE_ID + "/asset/temperature/feed"
+
 app = Flask(__name__)
-CORS(app, resources=r'/*')
+app.config['SECRET'] = SECRET
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MQTT_BROKER_URL'] = BROKER_URL
+app.config['MQTT_BROKER_PORT'] = BROKER_PORT
+app.config['MQTT_USERNAME'] = ''
+app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_TLS_ENABLED'] = False
+app.config['MQTT_CLEAN_SESSION'] = True
 
-app.config['SECRET_KEY'] = 'cQfTjWnZr4u7x!A%D*G-KaPdSgUkXp2s'
-socketio = SocketIO(app, cors_allowed_origins='*')
+# Parameters for SSL enabled
+# app.config['MQTT_BROKER_PORT'] = 8883
+# app.config['MQTT_TLS_ENABLED'] = True
+# app.config['MQTT_TLS_INSECURE'] = True
+# app.config['MQTT_TLS_CA_CERTS'] = 'ca.crt'
 
-@socketio.on('connect')
-def test_connect():
-    print("Device connected")
-    emit('temperature',
-         {'data': 'Print this out via data.data in your client'})
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+bootstrap = Bootstrap(app)
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("[INFO] - Connected to: " + BROKER_URL + str(rc))
+mqtt.subscribe(ATT_TEMP_SUB_TOPIC)
 
-    client.subscribe(ATT_TEMP_SUB_TOPIC)
-    print("[INFO] - Subscribed to: " + ATT_TEMP_SUB_TOPIC);
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload) + "\n")
-    emit('temperature', {'data': str(msg.payload)})
+@socketio.on('publish')
+def handle_publish(json_str):
+    pass
 
-        
-client = mqtt.Client()  
-client.username_pw_set(ATT_DEVICE_TOKEN, password='xxxxx')
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect(BROKER_URL, BROKER_PORT, 60)
+@socketio.on('subscribe')
+def handle_subscribe(json_str):
+   pass
+
+
+@socketio.on('unsubscribe_all')
+def handle_unsubscribe_all():
+    mqtt.unsubscribe_all()
+
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    print(data)
+    # socketio.emit('mqtt_message', data=data)
+
+
+# @mqtt.on_log()
+# def handle_logging(client, userdata, level, buf):
+#     print(level, buf)
+
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
-    client.loop_forever()
+    # important: Do not use reloader because this will create two Flask instances.
+    # Flask-MQTT only supports running with one instance
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=True)
