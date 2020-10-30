7680aaada86bbd18d9d3922bfe4437615bac45b5
Jens Vanhulst <vanhulstjens@gmail.com>
2020-10-30 21:32:10 +0100

    cleaned up repo

--- hardware/AirQuality/keys.h
+++
@@ -1,9 +0,0 @@
-/****
- * Enter your AllThingsTalk device credentials below
- */
-#ifndef KEYS_h
-#define KEYS_h
-const char* DEVICE_ID = "fO7K0gYRpEMJmY1PDy5cSpzh";
-const char* DEVICE_TOKEN = "maker:4H0f8zZfr9kSG0lqFujuiTclythuwmZx1ZNiMo8";
-const char* APN = "starter.att.iot";
-#endif
