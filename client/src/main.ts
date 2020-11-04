c15c2b21e05ad795ee6096657bbdc20a3619870c
SheldonPi1999 <55874520+SheldonPi1999@users.noreply.github.com>
2020-11-04 21:48:46 +0100

    Update

--- client/src/main.ts
+++ client/src/main.ts
@@ -11,7 +11,7 @@ import router from './router';
 import vuetify from './plugins/vuetify';
 
 Vue.prototype.$http = axios;
-Vue.prototype.$API_URL = 'wss://0.0.0.0';
+Vue.prototype.$API_URL = `${window.location.protocol}//${document.domain}:${window.location.port}`;
 Vue.config.productionTip = false;
 
 Vue.component('apexchart', VueApexCharts);
