7750efb234493ebf374ddb8936a8185a4f61fda5
Jens Vanhulst <vanhulstjens@gmail.com>
2020-11-13 13:28:32 +0100

    debug

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-heroku create attdbtest
+#heroku create attdbtest
 
-heroku addons:create heroku-postgresql:hobby-dev -a attdbtest
+# heroku addons:create heroku-postgresql:hobby-dev -a attdbtest
 
-heroku git:remote -a attdbtest
+# heroku git:remote -a attdbtest
 
 git subtree push --prefix server heroku master
