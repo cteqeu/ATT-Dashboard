6fda70650d5faa79f32d33446e1c0fe30484273b
Jens Vanhulst <vanhulstjens@gmail.com>
2020-11-13 22:14:21 +0100

    removed offline check

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,18 +3,12 @@
         <v-app-bar app color="primary" dark>
             <v-toolbar-title>{{ this.$route.name }}</v-toolbar-title>
             <v-spacer />
-            <v-btn v-if="$route.name === 'Charts'" @click="downloadCSV()" color="white" text
-                >Download csv</v-btn
-            >
+            <v-btn v-if="$route.name === 'Charts'" @click="downloadCSV()" color="white" text>
+                Download csv
+            </v-btn>
         </v-app-bar>
 
         <navigation-drawer :items="items" />
-
-        <Offline @detected-condition="handleConnectivityChange" />
-
-        <v-snackbar v-model="onlineStatus" bottom color="red" text timeout="-1">
-            You are disconnected.
-        </v-snackbar>
 
         <v-main>
             <v-container fluid>
@@ -28,14 +22,12 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import Offline from 'v-offline';
 import { NavItems } from './types';
 import NavigationDrawer from './components/NavigationDrawer.vue';
 
 @Component({
     components: {
         NavigationDrawer,
-        Offline,
     },
 })
 export default class App extends Vue {
@@ -45,17 +37,7 @@ export default class App extends Vue {
         { title: 'ATT - Orange', icon: 'mdi-web', link: '/att' },
     ];
 
-    onlineStatus = false;
-
     public downloadCSV = () => window.open('http://localhost:3000/api/download', '_blank');
-
-    handleConnectivityChange(status: boolean) {
-        if (!status) {
-            this.onlineStatus = true;
-        } else {
-            this.onlineStatus = false;
-        }
-    }
 }
 </script>
 
